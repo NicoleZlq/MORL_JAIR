75b3247fda06b2b6acf39a274b29c6196a1db7f4
NicoleZlq <um.zhangliqing@gmail.com>
2024-09-08 21:06:17 +0800

    '1'

--- MORL-master - update/multimario/scripts/_local/train_naive.sh
+++
@@ -1,9 +0,0 @@
-python run_n3c.py \
-	--env-id SuperMarioBros-v2 \
-	--use-gae \
-	--life-done \
-	--single-stage \
-	--training \
-	--standardization \
-	--num-worker 2 \
-	--sample-size 2 \
--- MORL-master - update/multimario/scripts/_workstation/train_base.sh
+++
@@ -1,9 +0,0 @@
-python run_a3c.py \
-	--env-id SuperMarioBros-v2 \
-	--use-cuda \
-	--use-gae \
-	--life-done \
-	--single-stage \
-	--training \
-	--standardization \
-	--num-worker 10 \
